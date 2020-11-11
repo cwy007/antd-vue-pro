00fd8df587d8354f11766d000170f3487f122a11
chenweiyan <chanweiyan007@gmail.com>
2020-11-11 17:39:52 +0800

    404

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import NotFound from "../views/404.vue";
 
 Vue.use(VueRouter);
 
@@ -88,17 +88,9 @@ const routes = [
     ]
   },
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "*",
+    name: "404",
+    component: NotFound
   }
 ];
 
