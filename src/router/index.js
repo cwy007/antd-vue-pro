510198d61b17a7fb75bd03dfc53b18d330fea918
chenweiyan <chanweiyan007@gmail.com>
2020-11-12 19:27:05 +0800

    getMenuData

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/user",
+    hideInMenu: true,
     component: () => import(/* webpackChunkName */ "../layouts/UserLayout.vue"),
     children: [
       {
@@ -39,11 +40,13 @@ const routes = [
       {
         path: "/dashboard",
         name: "dashboard",
+        meta: { icon: "dashboard", title: "仪表盘" },
         component: { render: h => h("router-view") },
         children: [
           {
             path: "/dashboard/analysis",
             name: "analysis",
+            meta: { title: "分析页" },
             component: () =>
               import(/* webpackChunkName: "dashboard" */ "../views/Dashboard/Analysis"),
           },
@@ -53,16 +56,20 @@ const routes = [
       {
         path: "/form",
         name: "form",
+        meta: { icon: "form", title: "表单" },
         component: { render: h => h("router-view") },
         children: [
           {
             path: "form/basic-form",
             name: "basicform",
+            meta: { title: "基础表单" },
             component: () => import(/* webpackChunkName: "form" */ "../views/Forms/BasicForm"),
           },
           {
             path: "/form/step-form",
             name: "stepform",
+            hideChildrenInMenu: true,
+            meta: { title: "分步表单" },
             component: () => import(/* webpackChunkName: "form" */ "../views/Forms/StepForm"),
             children: [
               {
@@ -96,6 +103,7 @@ const routes = [
   {
     path: "*",
     name: "404",
+    hideInMenu: true,
     component: NotFound,
   },
 ];
