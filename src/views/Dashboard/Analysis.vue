bd3804fb22e29a145bd84a3cbf54bfc0e9ff148b
chenweiyan <chanweiyan007@gmail.com>
2020-11-18 17:06:38 +0800

    mock 数据

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -3,35 +3,44 @@
 </template>
 
 <script>
-import random from "lodash/random";
+import request from "../../utils/request";
 import Chart from "../../components/Chart";
 export default {
   data() {
     return {
-      chartOption: {
-        title: {
-          text: "ECharts 入门示例",
-        },
-        tooltip: {},
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20],
-          },
-        ],
-      },
+      chartOption: {},
     };
   },
   mounted() {
-    this.interval = setInterval(() => {
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(() => random(100));
-      this.chartOption = { ...this.chartOption }; // 不使用深度监听时，使用一个新值
-    }, 3000);
+    this.getChartData();
+    this.interval = setInterval(this.getChartData(), 3000);
+  },
+  methods: {
+    getChartData() {
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { ID: 12345 },
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "Echart 入门示例",
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data,
+            },
+          ],
+        };
+      });
+    },
   },
   beforeDestroy() {
     clearInterval(this.interval);
--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,28 @@ module.exports = {
       },
     },
   },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        bypass: function(req, res) {
+          if (req.headers.accept.indexOf("html") !== -1) {
+            console.log("Skipping proxy for browser request.");
+            return "/index.html";
+          } else {
+            console.log("req.path", req.path);
+            const name = req.path
+              .split("/api/")[1]
+              .split("/")
+              .join("_");
+            const mock = require(`./mock/${name}`);
+            const result = mock(req.method);
+            console.log("result", result);
+            delete require.cache[require.resolve(`./mock/${name}`)];
+            return res.send(result);
+          }
+        },
+      },
+    },
+  },
 };
