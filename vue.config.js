94a85608d251ba58f475bc4ff277729dc2aef139
chenweiyan <chanweiyan007@gmail.com>
2020-11-18 18:00:20 +0800

    target: "http://localhost:8080"

--- vue.config.js
+++ vue.config.js
@@ -9,23 +9,24 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:8080",
         bypass: function(req, res) {
           if (req.headers.accept.indexOf("html") !== -1) {
             console.log("Skipping proxy for browser request.");
             return "/index.html";
           } else if (process.env.MOCK !== "none") {
-            console.log("req.path", req.path);
+            // console.log("req.path", req.path);
             const name = req.path
               .split("/api/")[1]
               .split("/")
               .join("_");
             const mock = require(`./mock/${name}`);
             const result = mock(req.method);
-            console.log("result", result);
+            // console.log("result", result);
             delete require.cache[require.resolve(`./mock/${name}`)];
             return res.send(result);
           }
+          return null;
         },
       },
     },
