f9a8b4255aa062a5502a222a9b59cb97a8c2c69b
chenweiyan <chanweiyan007@gmail.com>
2020-11-18 18:23:28 +0800

    普通表单

--- mock/dashboard_chart.js
+++ mock/dashboard_chart.js
@@ -4,7 +4,7 @@ function chart(method) {
   let res = null;
   switch (method) {
     case "GET":
-      res = [100, 40, 78, 10, 30, 48];
+      res = [90, 40, 78, 10, 30, 48];
       break;
     default:
       res = null;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button, Layout, Icon, Drawer, Radio, Menu } from "ant-design-vue";
+import { Button, Layout, Icon, Drawer, Radio, Menu, Form, Input } from "ant-design-vue";
 import Authorized from "./components/Authorized.vue";
 import Auth from "./directives/auth";
 
@@ -15,6 +15,8 @@ Vue.use(Icon);
 Vue.use(Drawer);
 Vue.use(Radio);
 Vue.use(Menu);
+Vue.use(Form);
+Vue.use(Input);
 
 // 全局注册权限验证组件
 Vue.component("Authorized", Authorized);
